9c40c327794cee5640c589b9fba69b8ec2367ef1
GwahelaWaks <116256261+GwahelaWaks@users.noreply.github.com>
2024-12-18 05:19:44 +0200

    Add files via upload

---
+++ Mibfa.OCR.Service.Service/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace Mibfa.OCR.Service.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new svcOCR() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
