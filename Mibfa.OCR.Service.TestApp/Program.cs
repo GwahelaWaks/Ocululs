e3f48026b56a193aff6213965e2a9393623bb270
GwahelaWaks <116256261+GwahelaWaks@users.noreply.github.com>
2024-12-18 10:15:00 +0200

    Add files via upload

---
+++ Mibfa.OCR.Service.TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Mibfa.OCR.Service.TestApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
