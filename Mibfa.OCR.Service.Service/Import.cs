7fc88e3ce4367cedaf0c69dc9b62004f4ebe6cda
GwahelaWaks <116256261+GwahelaWaks@users.noreply.github.com>
2024-12-18 15:11:28 +0200

    Add files via upload

---
+++ Mibfa.OCR.Service.Service/Import.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceProcess;
+
+namespace Mibfa.OCR.Service.Service
+{
+    public partial class svcOCR : ServiceBase
+    {
+        public svcOCR()
+        {
+            InitializeComponent();
+        }
+
+        //private   Europcar.License.Processor.Library.LicenseProcessing m_oOculusDriversLic;
+        private Mibfa.OCR.Service.Library.Library m_OculusOCR;
+        protected override void OnStart(string[] args)
+        {
+
+            m_OculusOCR = new Mibfa.OCR.Service.Library.Library();
+            m_OculusOCR.StartProcessing();
+
+        }
+
+        protected override void OnStop()
+        {
+            m_OculusOCR.StopProcessing();
+
+        }
+    }
+}
